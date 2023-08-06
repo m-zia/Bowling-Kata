ac447ba58ddcae5e535c3590f131da35f07d8c47
M Zia <murtaza_zia@hotmail.co.uk>
2023-08-06 17:04:09 +0100

    first iteration of code, calculating score for all nines and misses

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -1,9 +1,14 @@
-import { add } from "../src/calc";
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(add(10, 5)).toBe(15);
+//
+
+import { calculateScore } from "../src/calc";
+
+
+describe("Calculate Bowling Scores", () => {
+
+  it('calculates score for all nines and misses', () => {
+    const rolls = [9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0];
+    expect(calculateScore(rolls)).toBe(90);
   });
-  it("should return 5 for add(2,3)", () => {
-    expect(add(2, 3)).toBe(5);
-  });
+
+  
 });
--- src/calc.ts
+++ src/calc.ts
@@ -1,3 +1,20 @@
-export function add(x: number, y: number): number {
-  return x + y;
+//Ten-pin bowling
+//3 main cases to consider: strike, spare, normal
+// If it's a strike, you also get the bonus of the next 2 frames.
+// If it's a spare (10 obtained from the sum of two frames), you also get the bonus of the next frame after those two.
+
+
+//let rolls = [5,2]
+
+export function calculateScore(rolls: number[]): number {
+ 
+ let score = 0;
+
+ 
+  rolls.forEach(roll => {
+    score += roll
+  })
+
+  return score;
+
 }
